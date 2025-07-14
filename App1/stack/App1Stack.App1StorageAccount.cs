002832629a6e61a7f2779dc332c3492e72b51186
Joud Shaieb <joudshaieb524@gmail.com>
2025-07-14 19:43:18 +0400

    fix: str val

--- App1/stack/App1Stack.App1StorageAccount.cs
+++ App1/stack/App1Stack.App1StorageAccount.cs
@@ -3,6 +3,7 @@ using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
 using App1.helpers;
+using System;
 
 namespace App1.stack
 {
@@ -16,18 +17,69 @@ namespace App1.stack
                 ResourceGroupName = resourceGroup.Name,
                 Location = resourceGroup.Location,
                 Tags = deploymentConfigs.CommonTags,
-                Kind = Enum.Parse<Kind>(deploymentConfigs.StorageAccount["Kind"].ToString()!),
+                Kind = ParseKind(deploymentConfigs.StorageAccount["Kind"].ToString()!),
                 Sku = new SkuArgs
                 {
-                    Name = Enum.Parse<SkuName>(deploymentConfigs.StorageAccount["SkuName"].ToString()!)
+                    Name = ParseSkuName(deploymentConfigs.StorageAccount["SkuName"].ToString()!)
                 },
-                AccessTier = Enum.Parse<AccessTier>(deploymentConfigs.StorageAccount["AccessTier"].ToString()!),
+                AccessTier = ParseAccessTier(deploymentConfigs.StorageAccount["AccessTier"].ToString()!),
                 AllowBlobPublicAccess = bool.Parse(deploymentConfigs.StorageAccount["AllowBlobPublicAccess"].ToString()!),
-                MinimumTlsVersion = Enum.Parse<MinimumTlsVersion>(deploymentConfigs.StorageAccount["MinimumTlsVersion"].ToString()!),
+                MinimumTlsVersion = ParseMinimumTlsVersion(deploymentConfigs.StorageAccount["MinimumTlsVersion"].ToString()!),
                 EnableHttpsTrafficOnly = bool.Parse(deploymentConfigs.StorageAccount["EnableHttpsTrafficOnly"].ToString()!)
             });
 
             return storageAccount;
+        }
+
+        // Helper methods to parse the enum values safely
+        private static Kind ParseKind(string value)
+        {
+            return value switch
+            {
+                "StorageV2" => Kind.StorageV2,
+                "Storage" => Kind.Storage,
+                "BlobStorage" => Kind.BlobStorage,
+                "BlockBlobStorage" => Kind.BlockBlobStorage,
+                "FileStorage" => Kind.FileStorage,
+                _ => Kind.StorageV2 // Default value
+            };
+        }
+
+        private static SkuName ParseSkuName(string value)
+        {
+            return value switch
+            {
+                "Standard_LRS" => SkuName.Standard_LRS,
+                "Standard_GRS" => SkuName.Standard_GRS,
+                "Standard_RAGRS" => SkuName.Standard_RAGRS,
+                "Standard_ZRS" => SkuName.Standard_ZRS,
+                "Premium_LRS" => SkuName.Premium_LRS,
+                "Premium_ZRS" => SkuName.Premium_ZRS,
+                "Standard_GZRS" => SkuName.Standard_GZRS,
+                "Standard_RAGZRS" => SkuName.Standard_RAGZRS,
+                _ => SkuName.Standard_LRS // Default value
+            };
+        }
+
+        private static AccessTier ParseAccessTier(string value)
+        {
+            return value switch
+            {
+                "Hot" => AccessTier.Hot,
+                "Cool" => AccessTier.Cool,
+                _ => AccessTier.Hot // Default value
+            };
+        }
+
+        private static MinimumTlsVersion ParseMinimumTlsVersion(string value)
+        {
+            return value switch
+            {
+                "TLS1_0" => MinimumTlsVersion.TLS1_0,
+                "TLS1_1" => MinimumTlsVersion.TLS1_1,
+                "TLS1_2" => MinimumTlsVersion.TLS1_2,
+                _ => MinimumTlsVersion.TLS1_2 // Default value
+            };
         }
     }
 }
